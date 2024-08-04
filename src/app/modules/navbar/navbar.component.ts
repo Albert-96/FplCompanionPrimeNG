ff66002cc8ed7d38275574c603e79659a91712fe
Albert-96 <albertantony1059@yahoo.com>
2024-08-04 18:07:41 +0530

    Navbar changed to PrimeNG

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [AppComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [AppComponent, RouterOutlet, RouterLink, RouterLinkActive, MenubarModule, RippleModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   title: string;
+  items: MenuItem[] | undefined;
 
   constructor() {
     this.title = AppComponent.title;
+  }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Fixtures',
+        route: '/fixtures'
+      },
+      {
+        label: 'Players',
+        route: '/players'
+      }
+    ]
   }
 }
